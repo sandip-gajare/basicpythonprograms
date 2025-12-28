301343c354912278c01123da401e923af7085af3
sandip-gajare <sandipgajare566@gmail.com>
2025-12-28 15:52:38 +0530

    add new file : python program to check the numbers is odd or even

---
+++ Python Program to check if a Number is Odd or Even.py
@@ -0,0 +1,8 @@
+#Python Program to check if a Number is Odd or Even
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")

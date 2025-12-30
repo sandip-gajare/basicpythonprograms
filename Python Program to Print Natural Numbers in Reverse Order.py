8f0bfdc02e73042127e200194e5acedecae318c2
sandip-gajare <sandipgajare566@gmail.com>
2025-12-30 21:50:42 +0530

    add new file

---
+++ Python Program to Print Natural Numbers in Reverse Order.py
@@ -0,0 +1,15 @@
+    #python program to print natural numbers in reverse order
+
+'''n = int(input("enter a number: "))
+
+# Print natural numbers from n to 1
+for i in range(n, 0, -1):
+    print(i)'''
+
+#python program to print natural numbers in reverse order using while loop
+
+n = int(input("enter a number: "))
+
+while n >= 1:
+    print(n)
+    n -= 1
